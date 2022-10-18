5589632cd98e5916df107e075405b5e9cf638bb8
Ristan Alfatha Aditara <ristanaa09@gmail.com>
2022-10-18 21:55:22 +0800

    gitignore

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.pxab5ho.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient("mongodb+srv://RistanAA:admin@cluster0.xldtokf.mongodb.net/?retryWrites=true&w=majority")
 db = client.dbsparta
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('login.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Brilly\Documents\Belajar\Bootcamp\Week 1 Mini Project\venv
+set VIRTUAL_ENV=D:\LearningX\Week 1\Week-1-Mini-Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
