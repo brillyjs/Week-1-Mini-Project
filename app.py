99273ce6162f54d0d8c871226fea2b0edf63b71e
Brilly JS <brillyjs009@gmail.com>
2022-10-18 20:29:37 +0800

    new

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
-app = Flask(_name_)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.pxab5ho.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
-if _name_ == '_main_':
+if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
